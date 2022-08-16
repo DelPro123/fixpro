cceec034aa62719fa80d49e85c61006292c78b98
johndel siriban <MY_NAME@example.com>
2022-08-16 19:10:04 +0800

    login updated

--- modal.js
+++ modal.js
@@ -4,6 +4,9 @@ var book_button = document.getElementById("open_book_modal");
 // Get the modal for Sign up
 var signup_modal = document.getElementById("signup_modal");
 var signup_button = document.getElementById("SignUp");
+// Get the modal for Sign up
+var login_modal = document.getElementById("login_modal");
+var login_button = document.getElementById("SignLog");
 //for Sign up Bonus
 var SignUpBonus = document.getElementById("signUp_Bonus");
 //--------------------------------------------------------------------------
@@ -18,13 +21,7 @@ span_button.onclick = function() {
   book_modal.style.display = "none";
   book_modal=location.reload();
 }
-// When the user clicks anywhere outside of the modal, close it for book
-window.onclick = function(event) {
-  if (event.target == book_modal) {
-    book_modal.style.display = "none";
-    book_modal=location.reload();
-  }
-}
+
 //--------------------------------------------------------------------------
 // Get the <span> element that closes the modal for Sign up
 var signup_close= document.getElementsByClassName("close1")[0];
@@ -37,16 +34,38 @@ signup_close.onclick = function() {
   signup_modal.style.display = "none";
   signup_modal=location.reload();
 }
-// When the user clicks anywhere outside of the modal, close it for Sign up
+//--------------------------------------------------------------------------
+// Get the <span> element that closes the modal for Sign up
+var login_close= document.getElementsByClassName("close2")[0];
+// When the user clicks on the button, open the modal for Sign up
+login_button.onclick = function() {
+  login_modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal for the Sign up
+login_close.onclick = function() {
+  login_modal.style.display = "none";
+  login_modal=location.reload();
+}
+//-------------------------------------------------------------------------
 window.onclick = function(event) {
+// When the user clicks anywhere outside of the modal, close it for Sign up
   if (event.target ==signup_modal) {
     signup_modal.style.display = "none";
     signup_modal=location.reload();
+  }
+// When the user clicks anywhere outside of the modal, close it for book
+  else if (event.target == book_modal) {
+    book_modal.style.display = "none";
+    book_modal=location.reload();
+  }
+  else if (event.target == login_modal) {
+    login_modal.style.display = "none";
+    login_modal=location.reload();
   }
 }
 //--------------------------------------------------------------------------
 // When the user clicks on the button, open the modal for Sign up Bonus
 SignUpBonus.onclick = function() {
   signup_modal.style.display = "block";
-
 }
+
